e9fcefb93b87d62bee21595da08ac717a9bc0803
aqi <8812488@gmail.com>
2023-05-17 15:06:13 +0800

    change d

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	_ "net/http/pprof"
 	"os"
 
-	"github.com/zu1k/proxypool/api"
-	"github.com/zu1k/proxypool/internal/app"
-	"github.com/zu1k/proxypool/internal/cron"
-	"github.com/zu1k/proxypool/internal/database"
-	"github.com/zu1k/proxypool/pkg/proxy"
+	"github.com/8812488/proxypool/api"
+	"github.com/8812488/proxypool/internal/app"
+	"github.com/8812488/proxypool/internal/cron"
+	"github.com/8812488/proxypool/internal/database"
+	"github.com/8812488/proxypool/pkg/proxy"
 )
 
 var configFilePath = ""
